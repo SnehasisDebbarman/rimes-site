8395da61b64a77e20531e9d76092ae8128367ede
mktgsfdclightning <snehasisdebbarman2016@gmail.com>
2024-09-21 23:51:04 +0530

    testing

--- src/components/landing/stat-grid.tsx
+++ src/components/landing/stat-grid.tsx
@@ -1,9 +1,28 @@
 "use client";
-import React from "react";
-import { motion } from "framer-motion";
-import SmoothFadingCircle from "./moving-ball";
+import React, { useRef, useMemo } from "react";
+import { motion, useInView } from "framer-motion";
 import Image from "next/image";
 import People from "@/assets/Images/grid/people.png";
+import SmoothFadingCircle from "./moving-ball";
+
+const animationVariants = [
+  {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+  },
+  {
+    hidden: { opacity: 0, x: -20 },
+    visible: { opacity: 1, x: 0 },
+  },
+  {
+    hidden: { opacity: 0, scale: 0.8 },
+    visible: { opacity: 1, scale: 1 },
+  },
+  {
+    hidden: { opacity: 0, rotate: -10 },
+    visible: { opacity: 1, rotate: 0 },
+  },
+];
 
 interface StatCardProps {
   value: string;
@@ -21,42 +40,88 @@ const StatCard: React.FC<StatCardProps> = ({
   background,
   valueColor = "text-white",
   descriptionColor = "text-white",
-}) => (
-  <motion.div
-    className={`p-4 h-full flex flex-col-reverse bg-cover bg-center relative ${className}`}
-    initial={{ opacity: 0, y: 20 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ duration: 0.5 }}
-  >
-    {typeof background === "string" ? (
-      <Image
-        src={background}
-        alt="Background"
-        layout="fill"
-        objectFit="cover"
-        quality={100}
-      />
-    ) : background ? (
-      React.createElement(background, { className: "absolute inset-0" })
-    ) : null}
-    <div className={`relative z-10`}>
-      <h3 className={`text-4xl font-bold ${valueColor}`}>{value}</h3>
-      <p className={`text-sm mt-2 ${descriptionColor}`}>{description}</p>
-    </div>
-  </motion.div>
-);
+}) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { amount: 0.7 }); // Changed to 0.7 for 70% visibility
+  const variant = useMemo(
+    () =>
+      animationVariants[Math.floor(Math.random() * animationVariants.length)],
+    []
+  );
+
+  return (
+    <motion.div
+      ref={ref}
+      className={`p-4 h-full flex flex-col-reverse bg-cover bg-center relative ${className}`}
+      initial={{ opacity: 0, x: -20 }}
+      animate={isInView ? { opacity: 1, x: 0 } : { opacity: 0, x: -20 }}
+      transition={{ duration: 1, delay: 0.5 }} // Added delay here
+    >
+      {typeof background === "string" && (
+        <Image
+          src={background}
+          alt="Background"
+          layout="fill"
+          objectFit="cover"
+          quality={75}
+          priority={false}
+          loading="lazy"
+        />
+      )}
+      {background &&
+        typeof background !== "string" &&
+        React.createElement(background, { className: "absolute inset-0" })}
+      <div className={`relative z-10`}>
+        <motion.h3
+          className={`text-4xl font-bold ${valueColor}`}
+          variants={variant}
+          initial="hidden"
+          animate={isInView ? "visible" : "hidden"}
+          transition={{ duration: 0.5, delay: 0.8 }} // Increased delay
+        >
+          {value}
+        </motion.h3>
+        <motion.p
+          className={`text-sm mt-2 ${descriptionColor}`}
+          variants={variant}
+          initial="hidden"
+          animate={isInView ? "visible" : "hidden"}
+          transition={{ duration: 0.5, delay: 0.9 }} // Increased delay
+        >
+          {description}
+        </motion.p>
+      </div>
+    </motion.div>
+  );
+};
 
 const StatsDashboard: React.FC = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { amount: 0.4 });
+
   return (
-    <div className="bg-white p-8 rounded-xl max-w-screen-lg justify-center ">
-      <h2 className="text-3xl font-bold text-emerald-900 mb-4 text-center">
+    <div
+      ref={ref}
+      className="bg-white p-8 rounded-xl max-w-screen-lg justify-center"
+    >
+      <motion.h2
+        className="text-3xl font-bold text-emerald-900 mb-4 text-center"
+        initial={{ opacity: 0, y: -20 }}
+        animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: -20 }}
+        transition={{ duration: 0.5 }}
+      >
         H2 Stats
-      </h2>
-      <p className="text-emerald-700 mb-6 text-center max-w-2xl mx-auto">
+      </motion.h2>
+      <motion.p
+        className="text-emerald-700 mb-6 text-center max-w-2xl mx-auto"
+        initial={{ opacity: 0, y: -20 }}
+        animate={isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: -20 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+      >
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
         eirmod tempor invidunt ut labore et dolo aliquyam erat, sed diam
         voluptua.
-      </p>
+      </motion.p>
 
       <div className="grid grid-cols-12 grid-rows-12 h-56">
         <StatCard
@@ -64,9 +129,18 @@ const StatsDashboard: React.FC = () => {
           description="Lorem ipsum dolor sit amet."
           className="col-span-4 row-span-12 bg-[#143A33] rounded-l"
         />
-        <div className="col-span-3 row-span-12 bg-emerald-200 overflow-hidden">
+        <motion.div
+          className="col-span-3 row-span-12 bg-emerald-200 overflow-hidden"
+          variants={{
+            hidden: { opacity: 0, y: 20 },
+            visible: { opacity: 1, y: 0 },
+          }}
+          initial="hidden"
+          animate={isInView ? "visible" : "hidden"}
+          transition={{ duration: 0.5, delay: 0.2 }}
+        >
           <SmoothFadingCircle />
-        </div>
+        </motion.div>
         <StatCard
           value="700K+"
           description="Lorem ipsum dolor sit amet."
